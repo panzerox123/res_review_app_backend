f55c02bf0e2793dba8cc68e09f1a5c09e47b60bd
Kunal Bhat <kunal.bhat2001@gmail.com>
2021-12-08 12:25:17 +0530

    more functionsssss

--- index.js
+++ index.js
@@ -126,6 +126,49 @@ app.post('/foodGivenRestaurant', async(req,res)=>{
     res.status(200).send(back.rows)
 })
 
+app.post('/deliveryPersonnelOrders', async(req,res)=>{
+    client = new Client();
+    client.connect(err => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('Connected to DB');
+        }
+    });
+    try {
+        back = await client.query(`select * from (orders o join delivery_personnel d on o.personnelid = d.personnelid) join login_credentials l on l.username = d.username where d.username = '${req.body.username}';`);
+        client.end()
+    } catch (err){
+        console.log(err)
+        res.status(404).send()
+        return
+    }
+    res.status(200).send(back.rows)
+})
+
+app.post('/placeOrder', async(req,res)=>{
+    client = new Client();
+    client.connect(err => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('Connected to DB');
+        }
+    });
+    try {
+        const back = await client.query(`INSERT INTO ORDERS (cost, address, status, customerid, personnelid) values(100, 'Example Address', 'Order placed', '${req.body.customerid}', 1) RETURNING *`)
+        for(var i = 0; i < req.body.food.length; i++){
+            ins_back = await client.query(`INSERT INTO FOOD_ORDERS values(${req.body.food[i].id},${back.rows[0]['orderid']}, ${req.body.food[i].quantity});`)
+        }
+        client.end()
+    } catch (err){
+        console.log(err)
+        res.status(404).send()
+        return
+    }
+    res.status(200).send()
+})
+
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
 })
